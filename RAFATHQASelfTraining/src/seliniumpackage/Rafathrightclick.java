65202bce19806acaa8d199bf059cdee7195ff93b
msher <msher@LAPTOP-I42TTBGV.mynetworksettings.com>
2024-03-20 12:06:09 -0400

    Rafath new classes

--- RAFATHQASelfTraining/src/seliniumpackage/Rafathrightclick.java
+++ RAFATHQASelfTraining/src/seliniumpackage/Rafathrightclick.java
@@ -1,10 +1,26 @@
 package seliniumpackage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Rafathrightclick {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
+	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver1.exe");
+	ChromeDriver browserObject = new ChromeDriver();
+	browserObject.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
+	Actions action=new Actions (browserObject);
+	WebElement rightclickelement=browserObject.findElement(By.xpath("/html/body/div/section/div/div/div/p/span"));
+	action.contextClick(rightclickelement).build().perform();
+		Thread.sleep(6000);
+		
 	}
-
 }
+
+
+
+
